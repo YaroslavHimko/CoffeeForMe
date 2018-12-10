bdbe29d3ea515e8a9f32008f85c8c6f7e2a89c0f
yaroslav.himko <yaroslav.himko@gmail.com>
2018-12-10 13:37:38 +0300

    Split beverage and user functionality

---
+++ Beverage.py
@@ -0,0 +1,41 @@
+import sqlite3
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("type")
+parser.add_argument("ingredients")
+parser.add_argument("price")
+args = parser.parse_args()
+
+
+class Beverage(object):
+
+    def __init__(self):
+        self.type = args.type
+        self.ingredients = args.ingredients
+        self.price = args.price
+
+    def file_writer(self):
+        f = open("bill.txt", "w")
+        f.write("Got beverage {} with {} for {}".format(self.type, self.ingredients, self.price))
+
+    def file_reader(self):
+        f = open("bill.txt", "r")
+        print(f.readline())
+
+    def db_worker(self):
+        conn = sqlite3.connect('coffeeforme.db')
+
+        c = conn.cursor()
+        ###c.execute("""CREATE TABLE beverage (id INTEGER PRIMARY KEY, type TEXT, ingredients TEXT, price TEXT)""")
+        c.execute("INSERT INTO beverage VALUES ({}, '{}', '{}','{}')".format(1, self.type, self.ingredients,
+                                                                             self.price))
+        c.execute("SELECT * from beverage")
+        print(c.fetchone())
+
+
+beverage = Beverage()
+
+beverage.db_worker()
+beverage.file_writer()
+beverage.file_reader()
--- User.py
+++ User.py
@@ -4,9 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("username")
 parser.add_argument("position")
-parser.add_argument("type")
-parser.add_argument("ingredients")
-parser.add_argument("price")
+
 args = parser.parse_args()
 
 
@@ -15,33 +13,18 @@ class User(object):
         self.username = args.username
         self.position = args.position
 
+    def db_worker(self):
+        conn = sqlite3.connect('coffeeforme.db')
 
-class Beverage(object):
-    def __init__(self):
-        self.type = args.type
-        self.ingredients = args.ingredients
-        self.price = args.price
+        c = conn.cursor()
+        ###c.execute("""CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, position TEXT)""")
 
-    def file_writer(self):
-        f = open("bill.txt", "w")
-        f.write("Got beverage {} with {} for {}".format(self.type, self.ingredients, self.price))
+        c.execute("INSERT INTO users VALUES ({}, '{}','{}')".format(1, self.username, self.position))
 
-    def file_reader(self):
-        f = open("bill.txt", "r")
-        print(f.readline())
+        c.execute("SELECT * from users")
+
+        print(c.fetchone())
 
 
 user = User()
-beverage = Beverage()
-
-beverage.file_writer()
-beverage.file_reader()
-
-
-conn = sqlite3.connect('coffeeforme.db')
-
-c = conn.cursor()
-###c.execute("""CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, position TEXT)""")
-c.execute("INSERT INTO users VALUES ({}, '{}','{}')".format(1, user.username, user.position))
-c.execute("SELECT * from users")
-print(c.fetchone())
+user.db_worker()
